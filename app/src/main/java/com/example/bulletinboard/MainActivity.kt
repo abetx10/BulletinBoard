93df301aa6b17d38359cf27851e68d1879daabd3
abetx10 <abetx10@gmail.com>
2022-08-29 17:41:08 +0100

    part_4_86

--- app/src/main/java/com/example/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/example/bulletinboard/MainActivity.kt
@@ -245,7 +245,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private fun getAdsFromCat(cat: String) {
         currentCategory = cat
-        firebaseViewModel.loadAllAdsFromCat(cat)
+        firebaseViewModel.loadAllAdsFromCat(cat, filterDb!!)
     }
 
     fun uiUpdate(user: FirebaseUser?) {
